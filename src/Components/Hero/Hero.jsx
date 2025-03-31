a0b9cf247da8f50111cc36d4df316c5fe7342a23
Neelesh Singh <128823481+Neeleshsn@users.noreply.github.com>
2025-03-31 16:26:43 +0530

    Update Hero.jsx

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -7,8 +7,8 @@ const Hero = () => {
     return (
         <div id='home' className='hero'>
             <img src={profile_img} alt="" />
-            <h1><span>I'm Nilesh Singh,</span> frontend developer based in USA.</h1>
-            <p>I am a Frontend Developer from California, USA with 10 years of experience in multiple comapines like Microsoft, Tesla and Aaple.</p>
+            <h1><span>I'm Nilesh Singh,</span> frontend developer based in INDIA.</h1>
+            <p>I am a Frontend Developer from California, INDIA with 10 years of experience in multiple comapines like Microsoft, Tesla and Aaple.</p>
             <div className="hero-action">
                 <div className="hero-connect"><AnchorLink className='anchor-link' offset={50} href='#contact'>Connect With Me</AnchorLink></div>
                 <div className="hero-resume">My resume</div>
